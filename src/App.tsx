8e450c167708cac57d472592f1eec4c1cb4a5d23
dilepanaravinda-art <dilepan.aravinda@gmail.com>
2026-09-21 13:31:01 +0530

    Ship Planvanta premium V4 visual system

--- src/App.tsx
+++ src/App.tsx
@@ -210,6 +210,7 @@ const planvantaCapabilities = [
 
 function PlanvantaProductPage(){
   // Product route intentionally shares Vantoryx visual language with the corporate site.
+  // V4 premium visual system is deployed with this component.
   return <main className='pv3'>
     <header className='pv3-nav'>
       <a href='/' className='pv3-vx'><img src='/resources/vantoryx-logo.png' alt='Vantoryx Technologies'/></a>
